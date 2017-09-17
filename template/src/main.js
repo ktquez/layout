56434217557bb189282992489a1fbc3c13346c8f
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2017-09-17 19:59:01 -0300

    Import and Install vue-head if user confirm

--- template/src/main.js
+++ template/src/main.js
@@ -2,9 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import router from './router'
+{{#vueHead}}
+import VueHead from 'vue-head'
+{{/vueHead}}
 import { VueExtendLayout, layout } from 'vue-extend-layout'
 
 Vue.config.productionTip = false
+{{#vueHead}}
+Vue.use(VueHead)
+{{/vueHead}}
 Vue.use(VueExtendLayout)
 
 /* eslint-disable no-new */
